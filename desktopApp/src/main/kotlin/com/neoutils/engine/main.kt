5c99a36cf7973ab42ef1e5d5fb132bdbd9e8a28f
irineu <mmmirineusilva@gmail.com>
2026-05-17 20:19:54 -0300

    Initial commit: empty project

---
+++ desktopApp/src/main/kotlin/com/neoutils/engine/main.kt
@@ -0,0 +1,13 @@
+package com.neoutils.engine
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "engine",
+    ) {
+        App()
+    }
+}
